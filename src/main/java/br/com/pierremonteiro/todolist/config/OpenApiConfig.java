0181fd0132a78f5793af15901483e2cc2287f4d6
PierreOF <marcos.pierre@upe.br>
2023-11-03 16:02:52 -0300

    Feat: Swagger Config

---
+++ src/main/java/br/com/pierremonteiro/todolist/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package br.com.pierremonteiro.todolist.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Pierre Monteiro",email = "marcos.pierre@upe.br",url = "https://www.linkedin.com/in/pierremont/"),description = "ToDoList usando java + Spring para estudos",title = "Java ToDoList",version = "1.0"),servers = {@Server(description = "local enviroment",url = "http://localhost:8080"),@Server(description = "production enviroment",url = "https://todolist-nz2u.onrender.com")},security = {@SecurityRequirement(name = "basicAuth")})
+@SecurityScheme(name = "basicAuth", description = "JWT auth description", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+
+public class OpenApiConfig {
+}
